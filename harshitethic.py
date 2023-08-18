2e080dc332cd956aad06944474148a233e119c13
TUIN-TUIN <142479063+TUIN-TUIN@users.noreply.github.com>
2023-08-18 10:49:29 +0530

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+4from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from datetime import datetime
 import json, os, string, sys, threading, logging, time, re, random
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "6383159857:AAHsL_5p2BMtmx1L_sNHxNKayNZeJmSs5W0"
 else:
     tgkey = tgenv
 print(tgenv)
